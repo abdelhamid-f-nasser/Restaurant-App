a52f9304ef65996764c54466e396186d03a43df8
AFawzy <fawzy3171@gmail.com>
2023-02-20 11:05:06 +0200

    feat(Data): Add create order datasource method

--- lib/data/data_source/order_data_source.dart
+++ lib/data/data_source/order_data_source.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:restaurant_app/data/data_source/base_data_source.dart';
 import 'package:restaurant_app/data/enums/index.dart';
 import 'package:restaurant_app/data/mapper_extension/order_mapper.dart';
@@ -8,8 +9,21 @@ import 'package:restaurant_app/data/model/index.dart';
 class OrderDataSource extends BaseDataSource {
   OrderDataSource({required super.firestoreInstance});
 
-  Stream<List<OrderModel>> retrieveFood() => firestoreInstance
-      .collection(FirestoreCollection.orders.path)
+  CollectionReference get _fireStoreOrdersCollection =>
+      firestoreInstance.collection(FirestoreCollection.orders.path);
+
+  Future<void> createOrder(
+    OrderModel order,
+  ) =>
+      _fireStoreOrdersCollection.add(order.toMap());
+
+  Future<List<OrderModel>> getOrders() async {
+    final postSnapshots = await _fireStoreOrdersCollection.get();
+
+    return postSnapshots.docs.map((snapshot) => OrderModel.fromSnapshot(snapshot)).toList();
+  }
+
+  Stream<List<OrderModel>> getOrdersStream() => _fireStoreOrdersCollection
       .withConverter<OrderModel>(
         fromFirestore: (snapshot, options) => OrderModel.fromSnapshot(snapshot),
         toFirestore: (value, options) => value.toMap(),
