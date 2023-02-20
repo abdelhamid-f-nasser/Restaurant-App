36a3a6daae965a03b661314a6ea55a9fe0ba3288
AFawzy <fawzy3171@gmail.com>
2023-02-20 21:16:45 +0200

    feat(Data): Implement FavouriteDataSource Methods

--- lib/data/data_source/user_favourites_data_source.dart
+++ lib/data/data_source/user_favourites_data_source.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:restaurant_app/data/data_source/base_data_source.dart';
 import 'package:restaurant_app/data/enums/firestore_collection_enum.dart';
+import 'package:restaurant_app/data/mapper_extension/index.dart';
+import 'package:restaurant_app/data/model/index.dart';
 
 class UserFavouritesDataSource extends BaseDataSource {
   UserFavouritesDataSource({required super.firestoreInstance});
@@ -8,5 +12,30 @@ class UserFavouritesDataSource extends BaseDataSource {
   CollectionReference get _fireStoreFavouriteFoodCollection =>
       firestoreInstance.collection(FirestoreCollection.favourite.path);
 
-  //Todo Add retrieval and storage of favourites
+  Future<void> addFavouriteForUser(
+    UserFavouritesModel userFavouritesModel,
+  ) async =>
+      _fireStoreFavouriteFoodCollection
+          .doc(userFavouritesModel.userId)
+          .set(userFavouritesModel.toMap());
+
+  Stream<List<FoodItemModel>> getFavoritesForUser(String userId) {
+    return _fireStoreFavouriteFoodCollection
+        .withConverter<UserFavouritesModel>(
+          fromFirestore: (snapshot, options) =>
+              UserFavouritesModel.fromSnapshot(snapshot),
+          toFirestore: (value, options) => value.toMap(),
+        )
+        .where('user_id', isEqualTo: userId)
+        .snapshots()
+        .transform(
+      StreamTransformer.fromHandlers(
+        handleData: (data, sink) {
+          data.docs.isNotEmpty
+              ? sink.add(data.docs.first.data().foodItemList ?? [])
+              : sink.add([]);
+        },
+      ),
+    );
+  }
 }
